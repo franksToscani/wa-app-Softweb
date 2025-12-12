66131fb64e27f8a9cda396666d163618ce8ecdeb
softweb <softweb@localhost>
2025-12-12 09:49:23 +0100

    *aggiunto token di  protezione per le api create. *FINE TIROCINIO DA SOFTWEB

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,24 @@ use App\Http\Controllers\Api\CategoryTypeController;
 use App\Http\Controllers\Api\PostApiController;
 use App\Http\Controllers\Api\MediaApiController;
 
+
+/**
+ *  middleware per protegere le route api gradite via laravel sanctum
+ */
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Tutte le rotte qui sotto richiedono l'header Bearer Token
+    // token: 1|3xslXATnFWHt8vFvvnuNrlgx720yX0APwZ8t5Tmfa2552aea
+
 // Posts API
 Route::get('/posts', [PostApiController::class, 'index']);
+Route::get('/categories/{categoryId}/posts', [PostApiController::class, 'byCategory']);
 Route::get('/posts/{id}', [PostApiController::class, 'show']);
 
 // Media API
 Route::get('/media', [MediaApiController::class, 'index']);
 Route::get('/media/{id}', [MediaApiController::class, 'show']);
+
+});
+
